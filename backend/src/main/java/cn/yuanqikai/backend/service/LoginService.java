c0c36700275ff6ed61b6bb95b3530929d5164709
袁其凯 <shadowyuanqk@gmail.com>
2021-03-18 17:33:13 +0800

    feat 新增登录Controller

--- backend/src/main/java/cn/yuanqikai/backend/service/LoginService.java
+++ backend/src/main/java/cn/yuanqikai/backend/service/LoginService.java
@@ -4,5 +4,5 @@ package cn.yuanqikai.backend.service;
  * @author yuanqikai
  * @date 2021-03-18 11:28
  */
-public interface LoginServiceImpl {
+public interface LoginService {
 }
--- backend/src/main/java/cn/yuanqikai/backend/service/impl/LoginServiceImpl.java
+++ backend/src/main/java/cn/yuanqikai/backend/service/impl/LoginServiceImpl.java
@@ -1,6 +1,10 @@
-package cn.yuanqikai.backend.service.impl;/**
+package cn.yuanqikai.backend.service.impl;
+
+import cn.yuanqikai.backend.service.LoginService;
+
+/**
  *
  * @author yuanqikai
  * @date 2021-03-18 11:28
- */public class LoginService {
+ */public class LoginServiceImpl implements LoginService {
 }
